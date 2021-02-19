94846797e250b4e728bfa95b99302e5fe29e733a
Hershiv Haria <hershiv.haria@ovoenergy.com>
2021-02-19 17:41:44 +0000

    Exercise 2

--- exercises/src/main/scala/com.innerproduct.ee/io/TickingClock.scala
+++ exercises/src/main/scala/com.innerproduct.ee/io/TickingClock.scala
@@ -2,9 +2,15 @@ package com.innerproduct.ee.io
 
 import cats.effect._
 
+import scala.concurrent.duration._
+
 object TickingClock extends IOApp {
   def run(args: List[String]): IO[ExitCode] =
     tickingClock.as(ExitCode.Success)
 
-  val tickingClock: IO[Unit] = ??? // <1>
+  val tickingClock: IO[Unit] = for {
+  _ <- IO(println(System.currentTimeMillis))
+  _ <- IO.sleep(1.second)
+  _ <- tickingClock
+  } yield ()// <1>
 }
